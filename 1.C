7564d41a713b2a260ea51886ebe170458164a843
Saïd GRAJA <161341118+EnseaStudent@users.noreply.github.com>
2025-01-11 23:28:29 +0100

    Create 1.C

---
+++ 1.C
@@ -0,0 +1,6 @@
+if (argc != 4) //condition sur le nmbre d'arguments  { 
+    printf("Usage : ./client <serveur> <port> <fichier>\n");
+    exit(EXIT_FAILURE);
+}
+
+//vérifie si le programme reçoit les arguments nécessaires 
